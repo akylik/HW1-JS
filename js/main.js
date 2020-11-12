287c1a7ce58874687d41421d7a9bb9b18f15628f
Kulyk Oleksandr <a_kylik@ukr.net>
2020-11-12 14:09:44 +0200

    fixed js

--- js/main.js
+++ js/main.js
@@ -19,15 +19,17 @@ const isEvenOrOdd = totalPriceRound % 2 === 0;
 console.log(`Чи є сума всіх товарів парним числом ${isEvenOrOdd}`);
 
 const cashPay = 500;
-const cashRemainder = Number(cashPay - totalPrice);
+const cashRemainder = cashPay - totalPrice;
 console.log(`Сума решти, при оплаті всіх товарів ${cashRemainder}`);
 
 const average = Number(totalPrice / 3).toFixed(2);
 console.log(`Середнє значення цін ${average}`);
 
-const percentDiscount = (Math.random() * 100) .toFixed(0) ;
+const min = 10;
+const max = 50;
+const percentDiscount = Math.round(Math.random() * (max - min + 1)) + min;
 console.log(`Випадкова знижка ${percentDiscount}%`);
-const priceWithDiscount = Number(totalPrice-(totalPrice * percentDiscount / 100)).toFixed(2);
+const priceWithDiscount = Number(totalPrice - (totalPrice * percentDiscount / 100)).toFixed(2);
 console.log(`Сума до сплати зі знижкою ${priceWithDiscount}`);
 
 const profit = Number(totalPrice / 2 - priceWithDiscount).toFixed(2);
